61614bda7ce32d6f3d9a7ddf1eb3db9d6eb7c6d1
Nikola-Chobanov <nikolachobanov1@gmail.com>
2022-04-07 21:56:46 +0100

    Add project files.

---
+++ Management/Forms/frmAlunos.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAlunos
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.btnPrimeiro = New System.Windows.Forms.Button()
+        Me.btnAnterior = New System.Windows.Forms.Button()
+        Me.btnProximo = New System.Windows.Forms.Button()
+        Me.btnUltimo = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(776, 186)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.Location = New System.Drawing.Point(12, 242)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(776, 186)
+        Me.Panel2.TabIndex = 1
+        '
+        'btnPrimeiro
+        '
+        Me.btnPrimeiro.Location = New System.Drawing.Point(12, 204)
+        Me.btnPrimeiro.Name = "btnPrimeiro"
+        Me.btnPrimeiro.Size = New System.Drawing.Size(48, 25)
+        Me.btnPrimeiro.TabIndex = 2
+        Me.btnPrimeiro.Text = "<<"
+        Me.btnPrimeiro.UseVisualStyleBackColor = True
+        '
+        'btnAnterior
+        '
+        Me.btnAnterior.Location = New System.Drawing.Point(66, 204)
+        Me.btnAnterior.Name = "btnAnterior"
+        Me.btnAnterior.Size = New System.Drawing.Size(32, 25)
+        Me.btnAnterior.TabIndex = 3
+        Me.btnAnterior.Text = "<"
+        Me.btnAnterior.UseVisualStyleBackColor = True
+        '
+        'btnProximo
+        '
+        Me.btnProximo.Location = New System.Drawing.Point(104, 204)
+        Me.btnProximo.Name = "btnProximo"
+        Me.btnProximo.Size = New System.Drawing.Size(32, 25)
+        Me.btnProximo.TabIndex = 4
+        Me.btnProximo.Text = ">"
+        Me.btnProximo.UseVisualStyleBackColor = True
+        '
+        'btnUltimo
+        '
+        Me.btnUltimo.Location = New System.Drawing.Point(142, 204)
+        Me.btnUltimo.Name = "btnUltimo"
+        Me.btnUltimo.Size = New System.Drawing.Size(48, 25)
+        Me.btnUltimo.TabIndex = 5
+        Me.btnUltimo.Text = ">>"
+        Me.btnUltimo.UseVisualStyleBackColor = True
+        '
+        'frmAlunos
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnUltimo)
+        Me.Controls.Add(Me.btnProximo)
+        Me.Controls.Add(Me.btnAnterior)
+        Me.Controls.Add(Me.btnPrimeiro)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "frmAlunos"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents btnPrimeiro As Button
+    Friend WithEvents btnAnterior As Button
+    Friend WithEvents btnProximo As Button
+    Friend WithEvents btnUltimo As Button
+End Class
---
+++ Management/Forms/frmAlunos.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Data.SqlClient
+Public Class frmAlunos
+    Public Shared Property Cod_Aluno As String
+    Public Shared Property Nome As String
+    Public Shared Property Endereco As String
+    Public Shared Property Localidade As String
+    Public Shared Property Cod_Postal As String
+    Public Shared Property Curso As String
+    Public Shared Property FLG_Ativo As Boolean
+
+    Private Sub frmAlunos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
