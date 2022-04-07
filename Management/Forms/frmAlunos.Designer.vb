44e9112623523b19b64917f20e0f22f87e158015
Nikola-Chobanov <nikolachobanov1@gmail.com>
2022-04-07 23:02:28 +0100

    Adding variables

--- Management/Forms/frmAlunos.Designer.vb
+++ Management/Forms/frmAlunos.Designer.vb
@@ -22,27 +22,152 @@ Partial Class frmAlunos
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.PanelTop = New System.Windows.Forms.Panel()
+        Me.lblCurso = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.lblCod_Postal = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.lblLocalidade = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.lblNome = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.lblEndereco = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.lblCod_Aluno = New System.Windows.Forms.Label()
+        Me.txtCod_Aluno = New System.Windows.Forms.TextBox()
+        Me.PanelBottom = New System.Windows.Forms.Panel()
         Me.btnPrimeiro = New System.Windows.Forms.Button()
         Me.btnAnterior = New System.Windows.Forms.Button()
         Me.btnProximo = New System.Windows.Forms.Button()
         Me.btnUltimo = New System.Windows.Forms.Button()
+        Me.lblRegistos = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PanelTop.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'Panel1
+        'PanelTop
         '
-        Me.Panel1.Location = New System.Drawing.Point(12, 12)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(776, 186)
-        Me.Panel1.TabIndex = 0
+        Me.PanelTop.Controls.Add(Me.PictureBox1)
+        Me.PanelTop.Controls.Add(Me.lblCurso)
+        Me.PanelTop.Controls.Add(Me.TextBox6)
+        Me.PanelTop.Controls.Add(Me.lblCod_Postal)
+        Me.PanelTop.Controls.Add(Me.TextBox5)
+        Me.PanelTop.Controls.Add(Me.lblLocalidade)
+        Me.PanelTop.Controls.Add(Me.TextBox4)
+        Me.PanelTop.Controls.Add(Me.lblNome)
+        Me.PanelTop.Controls.Add(Me.TextBox3)
+        Me.PanelTop.Controls.Add(Me.lblEndereco)
+        Me.PanelTop.Controls.Add(Me.TextBox2)
+        Me.PanelTop.Controls.Add(Me.lblCod_Aluno)
+        Me.PanelTop.Controls.Add(Me.txtCod_Aluno)
+        Me.PanelTop.Location = New System.Drawing.Point(12, 12)
+        Me.PanelTop.Name = "PanelTop"
+        Me.PanelTop.Size = New System.Drawing.Size(776, 186)
+        Me.PanelTop.TabIndex = 0
         '
-        'Panel2
+        'lblCurso
         '
-        Me.Panel2.Location = New System.Drawing.Point(12, 242)
-        Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(776, 186)
-        Me.Panel2.TabIndex = 1
+        Me.lblCurso.AutoSize = True
+        Me.lblCurso.Location = New System.Drawing.Point(51, 145)
+        Me.lblCurso.Name = "lblCurso"
+        Me.lblCurso.Size = New System.Drawing.Size(34, 13)
+        Me.lblCurso.TabIndex = 11
+        Me.lblCurso.Text = "Curso"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(95, 142)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(205, 20)
+        Me.TextBox6.TabIndex = 10
+        '
+        'lblCod_Postal
+        '
+        Me.lblCod_Postal.AutoSize = True
+        Me.lblCod_Postal.Location = New System.Drawing.Point(31, 119)
+        Me.lblCod_Postal.Name = "lblCod_Postal"
+        Me.lblCod_Postal.Size = New System.Drawing.Size(58, 13)
+        Me.lblCod_Postal.TabIndex = 9
+        Me.lblCod_Postal.Text = "Cod Postal"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(95, 116)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(205, 20)
+        Me.TextBox5.TabIndex = 8
+        '
+        'lblLocalidade
+        '
+        Me.lblLocalidade.AutoSize = True
+        Me.lblLocalidade.Location = New System.Drawing.Point(27, 93)
+        Me.lblLocalidade.Name = "lblLocalidade"
+        Me.lblLocalidade.Size = New System.Drawing.Size(59, 13)
+        Me.lblLocalidade.TabIndex = 7
+        Me.lblLocalidade.Text = "Localidade"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(95, 90)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(205, 20)
+        Me.TextBox4.TabIndex = 6
+        '
+        'lblNome
+        '
+        Me.lblNome.AutoSize = True
+        Me.lblNome.Location = New System.Drawing.Point(54, 41)
+        Me.lblNome.Name = "lblNome"
+        Me.lblNome.Size = New System.Drawing.Size(35, 13)
+        Me.lblNome.TabIndex = 5
+        Me.lblNome.Text = "Nome"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(95, 38)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(205, 20)
+        Me.TextBox3.TabIndex = 4
+        '
+        'lblEndereco
+        '
+        Me.lblEndereco.AutoSize = True
+        Me.lblEndereco.Location = New System.Drawing.Point(33, 67)
+        Me.lblEndereco.Name = "lblEndereco"
+        Me.lblEndereco.Size = New System.Drawing.Size(53, 13)
+        Me.lblEndereco.TabIndex = 3
+        Me.lblEndereco.Text = "Endereço"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(95, 64)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(205, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'lblCod_Aluno
+        '
+        Me.lblCod_Aluno.AutoSize = True
+        Me.lblCod_Aluno.Location = New System.Drawing.Point(33, 15)
+        Me.lblCod_Aluno.Name = "lblCod_Aluno"
+        Me.lblCod_Aluno.Size = New System.Drawing.Size(56, 13)
+        Me.lblCod_Aluno.TabIndex = 1
+        Me.lblCod_Aluno.Text = "Cod Aluno"
+        '
+        'txtCod_Aluno
+        '
+        Me.txtCod_Aluno.Location = New System.Drawing.Point(95, 12)
+        Me.txtCod_Aluno.Name = "txtCod_Aluno"
+        Me.txtCod_Aluno.Size = New System.Drawing.Size(47, 20)
+        Me.txtCod_Aluno.TabIndex = 0
+        '
+        'PanelBottom
+        '
+        Me.PanelBottom.Location = New System.Drawing.Point(12, 242)
+        Me.PanelBottom.Name = "PanelBottom"
+        Me.PanelBottom.Size = New System.Drawing.Size(776, 186)
+        Me.PanelBottom.TabIndex = 1
         '
         'btnPrimeiro
         '
@@ -80,27 +205,63 @@ Partial Class frmAlunos
         Me.btnUltimo.Text = ">>"
         Me.btnUltimo.UseVisualStyleBackColor = True
         '
+        'lblRegistos
+        '
+        Me.lblRegistos.AutoSize = True
+        Me.lblRegistos.Location = New System.Drawing.Point(196, 210)
+        Me.lblRegistos.Name = "lblRegistos"
+        Me.lblRegistos.Size = New System.Drawing.Size(57, 13)
+        Me.lblRegistos.TabIndex = 12
+        Me.lblRegistos.Text = "Registos : "
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(623, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(150, 150)
+        Me.PictureBox1.TabIndex = 12
+        Me.PictureBox1.TabStop = False
+        '
         'frmAlunos
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblRegistos)
         Me.Controls.Add(Me.btnUltimo)
         Me.Controls.Add(Me.btnProximo)
         Me.Controls.Add(Me.btnAnterior)
         Me.Controls.Add(Me.btnPrimeiro)
-        Me.Controls.Add(Me.Panel2)
-        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.PanelBottom)
+        Me.Controls.Add(Me.PanelTop)
         Me.Name = "frmAlunos"
         Me.Text = "Form1"
+        Me.PanelTop.ResumeLayout(False)
+        Me.PanelTop.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents Panel1 As Panel
-    Friend WithEvents Panel2 As Panel
+    Friend WithEvents PanelTop As Panel
+    Friend WithEvents PanelBottom As Panel
     Friend WithEvents btnPrimeiro As Button
     Friend WithEvents btnAnterior As Button
     Friend WithEvents btnProximo As Button
     Friend WithEvents btnUltimo As Button
+    Friend WithEvents lblCod_Aluno As Label
+    Friend WithEvents txtCod_Aluno As TextBox
+    Friend WithEvents lblCurso As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents lblCod_Postal As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents lblLocalidade As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents lblNome As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents lblEndereco As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents lblRegistos As Label
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- Management/Forms/frmAlunos.vb
+++ Management/Forms/frmAlunos.vb
@@ -1,5 +1,17 @@
 ﻿Imports System.Data.SqlClient
 Public Class frmAlunos
+    '---------------------------------------------
+    Dim sqlConnection As SqlConnection
+    Dim sqlCMD As SqlCommand
+    Dim strConnection As String
+    Dim strCMD As String
+    Dim mRow As Integer = 0
+    Dim Mode As Integer
+    Const Add As Integer = 0
+    Const Drop As Integer = 1
+    Const Update As Integer = 2
+    '---------------------------------------------
+    '---------------------------------------------
     Public Shared Property Cod_Aluno As String
     Public Shared Property Nome As String
     Public Shared Property Endereco As String
@@ -7,8 +19,17 @@ Public Class frmAlunos
     Public Shared Property Cod_Postal As String
     Public Shared Property Curso As String
     Public Shared Property FLG_Ativo As Boolean
-
-    Private Sub frmAlunos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    '---------------------------------------------
+    '---------------------------------------------
+    Private Sub LoadAlunos()
 
     End Sub
+    '---------------------------------------------
+    '---------------------------------------------
+    Private Sub frmAlunos_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadAlunos()
+
+    End Sub
+    '---------------------------------------------
+    '---------------------------------------------
 End Class
